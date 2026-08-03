83b29a455c712134e46ca4c9ebcbfeac82d495f9
leo <leo@gmail.com>
2026-08-03 16:56:33 +0530

    added ther files and its structure

---
+++ bank-config-server/bank-config-server/src/test/java/com/example/bank_config_server/BankConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
